3357683d61a1bc7227877597cea73a8687db9e32
Christof <bauer.christof@gmail.com>
2022-08-30 11:36:05 -0500

    CSS on booked

--- components/Overlay.js
+++ components/Overlay.js
@@ -8,10 +8,10 @@ const Overlay = () => {
   return (
     open ?
       <div className="overlay">
-        <h1>We found a driver for you!</h1>
+        <h1 className="text-4xl">We found a driver for you!</h1>
         <p>Approx. arrival time at your location:</p>
-        <h1>11min</h1>
-        <button onClick={() => setOpen(false)} className='btn' aria-label="Close">Close</button>
+        <h1 className="text-4xl">11min</h1>
+        <button onClick={() => setOpen(false)} className='btn bg-brand' aria-label="Close">Close</button>
       </div>
       :
       ``
--- pages/confirm/booked.js
+++ pages/confirm/booked.js
@@ -13,11 +13,11 @@ const Booked = () => {
 
     return (
         <>
-            <div className="bookedContainer">
+            <div className="flex flex-col items-center justify-center h-[calc(100vh-120px)]">
                 <svg className="w-24 h-24 text-teal-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <h1>Request Received</h1>
+                <h1 className='text-4xl mb-4'>Request Received</h1>
                 <p>We&apos;ll notify you as soon as we find a driver<br />Have a safe journey</p>
             </div>
             <p className='mt-20 block text-xs'>You&apos;ll be redirected in a few seconds</p>
